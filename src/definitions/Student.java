900a86fdc8b826076808f13deb5333bccdc17f15
HARSHIT BHARDWAJ <harshit.bhardwaj_cs19@gla.ac.in>
2020-08-30 14:53:43 +0530

    Create a parameterized constructor for the Student class

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -9,11 +9,16 @@ package definitions;
 import java.util.Scanner;
 
 public class Student {
-
     Scanner scanner = new Scanner(System.in);
     private String studentName;
     private long universityRollNumber;
     private Book[] nameOfBooks;
     private int numberOfBookIssued = 0;
 
+    public Student(String studentName, long universityRollNumber, Book[] nameOfBooks, int numberOfBookIssued) {
+        this.studentName = studentName;
+        this.universityRollNumber = universityRollNumber;
+        this.nameOfBooks = nameOfBooks;
+        this.numberOfBookIssued = numberOfBookIssued;
+    }
 }
